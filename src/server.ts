9e9b174407e5aa8bbe8009afc49f4b724fd951c9
denismedeirossdk <contato.denismedeiros@hotmail.com>
2021-01-26 16:36:56 -0300

    feat: add cors

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,4 @@
+import cors from 'cors'
 import express from 'express'
 
 import routes from './routes'
@@ -6,6 +7,8 @@ const PORT = process.env.PORT || 3333
 
 const app = express()
 app.use(express.json())
+
+app.use(cors())
 
 app.use(routes)
 
