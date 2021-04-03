d226fa3cf4cfcb02c56e411ddf3467a243f74d6b
denismedeirossdk <contato.denismedeiros@hotmail.com>
2021-04-03 16:58:45 -0300

    refactor: refactor server for future tests

---
+++ src/shared/infra/http/app.ts
@@ -0,0 +1,18 @@
+import 'reflect-metadata'
+import cors from 'cors'
+import express from 'express'
+
+import createConnection from '@shared/infra/typeorm'
+
+import routes from './routes'
+
+createConnection()
+
+const app = express()
+app.use(express.json())
+
+app.use(cors())
+
+app.use(routes)
+
+export { app }
