3556957a9765863632393d617aa439145d604e2c
denismedeirossdk <contato.denismedeiros@hotmail.com>
2021-04-04 16:02:27 -0300

    fix: fix name userUseCase in controller

--- src/modules/accounts/useCases/createUser/CreateUserController.ts
+++ src/modules/accounts/useCases/createUser/CreateUserController.ts
@@ -1,15 +1,15 @@
 import { Request, Response } from 'express'
 import { container } from 'tsyringe'
 
-import { CreateUseUserCase } from './CreateUserUseCase'
+import { CreateUserUseCase } from './CreateUserUseCase'
 
 class CreateUserController {
   async handle(request: Request, response: Response): Promise<Response> {
     const { name, email, password } = request.body
 
-    const createUseUserCase = container.resolve(CreateUseUserCase)
+    const createUserUseCase = container.resolve(CreateUserUseCase)
 
-    const user = await createUseUserCase.execute({ name, email, password })
+    const user = await createUserUseCase.execute({ name, email, password })
 
     return response.status(201).json(user)
   }
