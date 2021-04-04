b3c19680b54e1ddf8ce04e62e285c03785b2e36f
denismedeirossdk <contato.denismedeiros@hotmail.com>
2021-04-04 11:53:16 -0300

    feat: create in memory user
    create new user in memory

---
+++ src/modules/accounts/dtos/ICreateUserDTO.ts
@@ -0,0 +1,10 @@
+interface ICreateUserDTO {
+  id?: string
+  name: string
+  avatar?: string
+  phone?: string
+  email: string
+  password: string
+}
+
+export { ICreateUserDTO }
---
+++ src/modules/accounts/repositories/IUserRepository.ts
@@ -0,0 +1,9 @@
+import { ICreateUserDTO } from '../dtos/ICreateUserDTO'
+import { User } from '../infra/typeorm/entities/User'
+
+interface IUserRepository {
+  create(data: ICreateUserDTO): Promise<User>
+  findByEmail(email: string): Promise<User>
+}
+
+export { IUserRepository }
