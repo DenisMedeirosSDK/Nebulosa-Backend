c5ade029c7ed56fd37bc1aa88ce015e34c48d340
denismedeirossdk <contato.denismedeiros@hotmail.com>
2021-04-13 14:41:15 -0300

    refactor: mult disk storage

--- src/shared/container/providers/StorageProvider/index.ts
+++ src/shared/container/providers/StorageProvider/index.ts
@@ -3,7 +3,11 @@ import { container } from 'tsyringe'
 import { LocalStorageProvider } from './implementations/LocalStorageProvider'
 import { IStorageProvider } from './IStorageProvider'
 
+const diskStorage = {
+  local: LocalStorageProvider
+}
+
 container.registerSingleton<IStorageProvider>(
   'StorageProvider',
-  LocalStorageProvider
+  diskStorage[process.env.DISK || 'local']
 )
