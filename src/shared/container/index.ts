e59c62fd427828a3b34dd67e193761bc69df74b8
denismedeirossdk <contato.denismedeiros@hotmail.com>
2021-04-13 13:46:14 -0300

    feat: create dependencies

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,5 +1,7 @@
 import { container } from 'tsyringe'
 
+import '@shared/container/providers'
+
 import { UserRepository } from '@modules/accounts/infra/typeorm/repositories/UserRepository'
 import { IUserRepository } from '@modules/accounts/repositories/IUserRepository'
 import { CategoryRepository } from '@modules/services/infra/typeorm/repositories/CategoryRepository'
