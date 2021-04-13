053e93c8bb9e977efa7dd1836d3c2c165266c109
denismedeirossdk <contato.denismedeiros@hotmail.com>
2021-04-13 10:06:11 -0300

    refactor: rename .env and create example

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,7 +1,7 @@
 import { app } from './app'
 
-const PORT = process.env.PORT || 3333
+const PORT = process.env.APP_PORT || 3333
 
 app.listen(PORT, () => {
-  console.log('Server start on PORT')
+  console.log('Server start on APP_PORT')
 })
