52fbcc3bb87946db504a9e860f4ed5c3776fa4e8
denismedeirossdk <contato.denismedeiros@hotmail.com>
2021-04-13 11:26:16 -0300

    feat(babel): config babel compile
    config babel modules resolve and start script

--- babel.config.js
+++ babel.config.js
@@ -13,13 +13,16 @@ module.exports = {
   plugins: [
     ['module-resolver', {
       alias: {
-        '@shared/': './src/shared',
-        '@modules/': './src/modules',
-        '@config/': './src/config',
-        '@errors/': './src/errors',
-        '@utils/': './src/utils',
+        '@shared': './src/shared',
+        '@modules': './src/modules',
+        '@config': './src/config',
+        '@errors': './src/errors',
+        '@utils': './src/utils',
       }
     }],
+    "babel-plugin-transform-typescript-metadata",
+    ["@babel/plugin-proposal-decorators", { legacy: true }],
+    ["@babel/plugin-proposal-class-properties", { loose: true }],
   ],
   ignore: [
     '**/*.spec.ts'
