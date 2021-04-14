2c889fbccd527de7b2bd22a18b8159720ecddcef
denismedeirossdk <contato.denismedeiros@hotmail.com>
2021-04-14 20:27:10 -0300

    test: jest config
    coverage config

--- jest.config.js
+++ jest.config.js
@@ -19,7 +19,7 @@ module.exports = {
   collectCoverage: true,
 
   // An array of glob patterns indicating a set of files for which coverage information should be collected
-  collectCoverageFrom: ['<rootDir>/src/modules/**/useCases/**/*.ts'],
+  collectCoverageFrom: ['<rootDir>/src/modules/**/useCases/**/*.ts',],
 
   // The directory where Jest should output its coverage files
   coverageDirectory: 'coverage',
