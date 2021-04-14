06d76e880f46a91b7f4e878b6d4b42246b9fcc44
denismedeirossdk <contato.denismedeiros@hotmail.com>
2021-04-14 13:51:12 -0300

    feat: add new function in date provider

--- src/shared/container/providers/DateProvider/implementations/DateDaysJSProvider.ts
+++ src/shared/container/providers/DateProvider/implementations/DateDaysJSProvider.ts
@@ -3,6 +3,14 @@ import dayjs from 'dayjs'
 import { IDateProvider } from '../IDateProvider'
 
 class DateDaysJSProvider implements IDateProvider {
+  compareIfBefore(startDate: Date, endDate: Date): boolean {
+    return dayjs(startDate).isBefore(endDate)
+  }
+
+  dateNow(): Date {
+    return dayjs().toDate()
+  }
+
   addHours(hours: number): Date {
     return dayjs().add(hours, 'hours').toDate()
   }
