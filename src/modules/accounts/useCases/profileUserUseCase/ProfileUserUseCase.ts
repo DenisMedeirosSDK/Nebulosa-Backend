c27e45498d5aa993adbfc62fbda3fdb647b83303
denismedeirossdk <contato.denismedeiros@hotmail.com>
2021-04-14 20:25:37 -0300

    feat: check user exits

--- src/modules/accounts/useCases/profileUserUseCase/ProfileUserUseCase.ts
+++ src/modules/accounts/useCases/profileUserUseCase/ProfileUserUseCase.ts
@@ -3,6 +3,7 @@ import { inject, injectable } from 'tsyringe'
 import { IUserResponseDTO } from '@modules/accounts/dtos/IUserResponseDTO'
 import { UserMap } from '@modules/accounts/mapper/UserMap'
 import { IUserRepository } from '@modules/accounts/repositories/IUserRepository'
+import { AppError } from '@shared/errors/AppError'
 
 @injectable()
 class ProfileUserUseCase {
@@ -13,6 +14,10 @@ class ProfileUserUseCase {
 
   async execute(id: string): Promise<IUserResponseDTO> {
     const user = await this.userRepository.findById(id)
+
+    if (!user) {
+      throw new AppError('User does not exists!')
+    }
 
     return UserMap.toDTO(user)
   }
