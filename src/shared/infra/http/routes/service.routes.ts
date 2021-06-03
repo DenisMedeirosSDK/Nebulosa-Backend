05b0edf0f6498b99b9733104f0cd2ce21d413add
denismedeirossdk <contato.denismedeiros@hotmail.com>
2021-06-03 14:59:56 -0300

    feat: create auth service

--- src/shared/infra/http/routes/service.routes.ts
+++ src/shared/infra/http/routes/service.routes.ts
@@ -2,9 +2,13 @@ import { Router } from 'express'
 
 import { CreateServiceController } from '@modules/services/useCases/createServices/CreateServiceController'
 
+import { ensureAuthenticated } from '../middlewares/ensureAuthenticated'
+
 const createServiceController = new CreateServiceController()
 
 const serviceRoutes = Router()
+
+serviceRoutes.use(ensureAuthenticated)
 
 serviceRoutes.post('/', createServiceController.handle)
 
