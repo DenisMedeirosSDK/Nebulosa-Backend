c98a92d51b75203ed057b1d0d7847c36d99feeea
denismedeirossdk <contato.denismedeiros@hotmail.com>
2021-06-07 12:59:43 -0300

    feat: injection appointment

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -6,6 +6,8 @@ import { UserRepository } from '@modules/accounts/infra/typeorm/repositories/Use
 import { UserTokenRepository } from '@modules/accounts/infra/typeorm/repositories/UserTokenRepository'
 import { IUserRepository } from '@modules/accounts/repositories/IUserRepository'
 import { IUserTokenRepository } from '@modules/accounts/repositories/IUserTokenRepository'
+import { AppointmentRepository } from '@modules/appointment/infra/typeorm/repositories/AppointmentRepository'
+import { IAppointmentRepository } from '@modules/appointment/repositories/IAppointmentRepository'
 import { CategoryRepository } from '@modules/services/infra/typeorm/repositories/CategoryRepository'
 import { ServiceRepository } from '@modules/services/infra/typeorm/repositories/ServiceRepository'
 import { ICategoryRepository } from '@modules/services/repositories/ICategoryRepository'
@@ -29,4 +31,9 @@ container.registerSingleton<IUserTokenRepository>(
 container.registerSingleton<IServiceRepository>(
   'ServiceRepository',
   ServiceRepository
+)
+
+container.registerSingleton<IAppointmentRepository>(
+  'AppointmentRepository',
+  AppointmentRepository
 )
