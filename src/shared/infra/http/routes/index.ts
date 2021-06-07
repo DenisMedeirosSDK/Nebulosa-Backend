942ea01dbbe4fbd675e304dfe7994c546017ea12
denismedeirossdk <contato.denismedeiros@hotmail.com>
2021-06-07 12:58:22 -0300

    feat: route appointment

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express'
 
+import appointmentRoutes from './appointment.routes'
 import authenticateRoutes from './authenticate.routes'
 import categoryRoutes from './category.routes'
 import passwordRoutes from './password.routes'
@@ -11,6 +12,7 @@ const routes = Router()
 routes.use('/users', usersRoutes)
 routes.use('/category', categoryRoutes)
 routes.use('/services', serviceRoutes)
+routes.use('/appointments', appointmentRoutes)
 routes.use('/password', passwordRoutes)
 routes.use(authenticateRoutes)
 
