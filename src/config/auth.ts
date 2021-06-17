e81e8669b357ed97e4f54141c7aef4fd6bd2e66a
denismedeirossdk <contato.denismedeiros@hotmail.com>
2021-06-17 17:51:30 -0300

    fix: fix refresh token

--- src/config/auth.ts
+++ src/config/auth.ts
@@ -7,10 +7,3 @@ export default {
     expiresInRefreshTokenDays: Number(process.env.EXPIRES_REFRESH_TOKEN_DAYS)
   }
 }
-// jwt: {
-//   secretToken: process.env.SECRET_TOKEN,
-//   expiresInToken: parseInt(process.env.EXPIRES_IN_TOKEN),
-//   secretRefreshToken: process.env.SECRET_REFRESH_TOKEN,
-//   expiresInRefreshToken: parseInt(process.env.EXPIRES_IN_REFRESH_TOKEN),
-//   expiresInRefreshTokenDays: Number(process.env.EXPIRES_REFRESH_TOKEN_DAYS)
-// }
